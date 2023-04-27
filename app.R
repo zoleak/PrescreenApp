8fee3ebc58778e7903b85b754aa8f2b3236ad358
Zolea <Kevin.Zolea@dep.nj.gov>
2023-04-27 14:53:08 -0400

    added calendar functionality and test code to perform that and email to user/me

--- app.R
+++ app.R
@@ -278,9 +278,30 @@ observeEvent(input$submit, {
       # If user meets all criteria, show success message
       showModal(
         modalDialog(
-          title = "Application Submitted",
-          "Congratulations! You meet all our minimum criteria and are eligible for the rental property.",
-          easyClose = TRUE
+          "Congratulations! You meet all our minimum criteria and 
+          are eligible for the rental property.",
+          size = "l",
+          calendarInput(
+            inputId = "showing_calendar",
+            label = "Select a date and time for showing:",
+            weekends = TRUE,
+            editable = TRUE,
+            view = "week",
+            events = list(
+              # block out certain dates/times (e.g. Sundays)
+              list(
+                title = "Not available",
+                start = "2022-05-01",
+                dow = c(0)
+              ),
+              # add event for user-selected showing time
+              list(
+                title = "Showing",
+                start = "2022-05-10T12:00:00",
+                end = "2022-05-10T13:00:00"
+              )
+            )
+          )
         )
       )
     # If the criteria are not met, then the user will be shown an error message
@@ -299,5 +320,147 @@ observeEvent(input$submit, {
 
   
 }
-##################################################################################
+
+
+### THIS IS SO I DON'T LOOSE REST OF CODE
+
+#
+# Load necessary packages
+#library(shiny)
+#library(shinythemes)
+#library(shinyWidgets)
+#library(shinyjs)
+#library(blastula)
+#
+## Create HTML email template
+#email_body <- render_email_body({
+#  hero(
+#    title = "Your scheduled showing",
+#    subtitle = "Details",
+#    footer = "Thank you for choosing our rental property!"
+#  )
+#})
+#
+## define email settings
+#email_settings <- smtp_server(
+#  config = list(
+#    server = 'smtp.gmail.com',
+#    port = 465,
+#    username = '{your-email-address@gmail.com}',
+#    password = '{your-password}',
+#    ssl = TRUE
+#  )
+#)
+#
+## Define UI
+#ui <- fluidPage(
+#  theme = shinytheme("flatly"),
+#  shinyjs::useShinyjs(),
+#  # ...
+#  # existing UI elements
+#  # ...
+#  tags$head(tags$style(
+#    HTML("
+#         .mandatory_star { color: red; } 
+#         .fc-event { background-color: #4285F4; color: white; }
+#         ")
+#  )),
+#  titlePanel(div("Hinds Property Management",
+#                 style = 'background-color:#4285F4;
+#                           color:white;')),
+#  # ...
+#  # existing UI elements
+#  # ...
+#  fluidRow(tags$h1(
+#    class = "centered",
+#    HTML("<u>Questionnaire for Rental Property</u>")
+#  )),
+#  # ...
+#  # existing UI elements
+#  # ...
+#  actionButton(inputId = "submit",
+#               label = "Submit",
+#               class = "btn-primary",
+#               width = "100%")
+#)
+#
+## Define server
+#server <- function(input, output, session) {
+#  # ...
+#  # existing server code
+#  # ...
+#  
+#  # render interactable calendar in success modal for qualified users
+#  observeEvent(input$submit, {
+#    if (input$honesty == "Yes I will" &&
+#        input$income == "Yes I do" &&
+#        input$income_amt == "Yes it is" &&
+#        input$credit_score == "Above 600" &&
+#        input$past_landlords == "Yes I am" &&
+#        input$great_ref == "Yes I will" &&
+#        input$evicted == "No I haven't" &&
+#        input$background_check == "Yes I am" &&
+#        input$smoke == "No" &&
+#        input$pets == "No") {
+#      # render calendar
+#      showModal(
+#        modalDialog(
+#          title = "Application Submitted",
+#          "Congratulations! You meet all our minimum criteria and are eligible for the rental property.
+#          Please choose a date and time for a showing",
+#          easyClose = TRUE,
+#          calendarInput(
+#            inputId = "showing_calendar",
+#            label = "Select a date and time for showing:",
+#            weekends = TRUE,
+#            editable = TRUE,
+#            view = "week",
+#            events = list(
+#              # block out certain dates/times (e.g. Sundays)
+#              list(
+#                title = "Not available",
+#                start = "2022-05-01",
+#                dow = c(0)
+#              ),
+#              # add event for user-selected showing time
+#              list(
+#                title = "Showing",
+#                start = "2022-05-10T12:00:00",
+#                end = "2022-05-10T13:00:00"
+#              )
+#            )
+#          ),
+#          footer = modalButton(
+#            "Schedule",
+#            onclick = jsCode("
+#                            var event = $('#showing_calendar').fullCalendar('clientEvents')[0];
+#                            var startDate = event.start.format('dddd, MMMM Do YYYY, h:mm a'); 
+#                            $('#showing_calendar').fullCalendar('removeEvents');
+#                            // send email to user
+#                            send_email(
+#                              recipient = input$email,
+#                              body = '<p>Thank you for scheduling a showing! The details of your appointment are:</p>' + startDate,
+#                              subject = 'Rental property showing'
+#                            )
+#                            // send email to you
+#                            send_email(
+#                              recipient = '{your-email-address@gmail.com}',
+#                              body = '<p>A showing has been scheduled. The details of the appointment are:</p>' + startDate,
+#                              subject = 'Rental property showing'
+#                            )
+#                            Shiny.unbindAll(this); $(this).remove();")
+#          ),
+#          ignoreCancel = TRUE
+#        )
+#      )
+#      # ...
+#      # existing server code
+#      # ...
+#    }
+#  })
+#
+#
+##
+###################################################################################
 shinyApp(ui, server)
+#
